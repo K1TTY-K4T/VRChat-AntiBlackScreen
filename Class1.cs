efeafb86d46e879a7dac95af4cfe84397fe5c18d
Kat <59918200+K1TTY-K4T@users.noreply.github.com>
2020-10-25 15:51:52 -0400

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,9 @@
+﻿using MelonLoader;
+using UnityEngine;
+namespace kittys_anti_blackscreen{
+    public class Class1:MelonMod{
+        public override void VRChat_OnUiManagerInit(){
+            GameObject.Find("UserInterface/PlayerDisplay/BlackFade/inverted_sphere").SetActive(false);
+        }
+    }
+}
